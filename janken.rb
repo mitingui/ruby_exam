ccb69889237465c9a1b1243e0f791f980e327a71
mitingui <mitinguiars@gmail.com>
2022-02-24 15:45:59 +0100

    correction ruby_exam

--- janken.rb
+++ janken.rb
@@ -1,43 +1,40 @@
+
 class Player
+  
   def hand
     # プレイヤーにじゃんけんの手を選択させる文章を表示させます。
     puts "数字を入力してください。"
-    puts "0:グー"
-    puts "1:チョキ"
-    puts "2:パー"
-   
+    puts "0:pierre"
+    puts "1:papier"
+    puts "2:ciseaux"
     puts
     puts
-    puts
-    # 変数「input_hand」にプレイヤーの入力値を代入します。
-    # ヒント：getsメソッドについて調べてみましょう。
-    input_hand = gets.to_i
+    input_hand = gets
+
+    #On vérifie si le player a entré soit 0, 1 ou 2
+    #Lorque on introduit une lettre et on converti on obtient zero
+    #Donc il n'y a vait pas le moyen de savoir si c'est une lettre ou un zero lorsque on converti
+    #C"est pour cela qu'on prend la valeur en ASCII pour savoir si on est en présence que de 0, 1 ou 2
+    if input_hand.unpack('U*')[0] == 48
+      input_hand = 0
+    elsif input_hand.unpack('U*')[0] == 49
+      input_hand = 1
+    elsif input_hand.unpack('U*')[0] == 50
+      input_hand = 2
+    end
+
     bool = true
-    # 「input_hand」が「0, 1, 2」のいずれかだと繰り返し処理を終了し、それ以外（アルファベットも含む）だと繰り返し処理を継続します。
-    while true
+    while bool
         bool = (input_hand == 0 || input_hand == 1 || input_hand == 2)
-         # if 「input_hand」が「0, 1, 2」のいずれかの場合だった場合
-      # ヒント：include?メソッドについて調べてみましょう。
-        # 「input_hand」をそのまま返す。
-        # ヒント：戻り値を返して繰り返し処理を終了させたい場合、「return」を使用します。
        if bool
         return input_hand
        else
-        # puts "0〜2の数字を入力してください。"
-        # puts "0:グー, 1:チョキ, 2:パー"
+        print "\e[2J\e[f" #On efface l'écran
+        puts "Veuillez saisir un nombre entre 0 et 2"
         puts
-        puts "0〜2の数字を入力してください。"
-        puts "0:グー"
-        puts "1:チョキ"
-        puts "2:パー"
-        input_hand = gets.to_i
+        jeux = Player.new
+        jeux.hand
        end
-      # else それ以外の場合
-        # プレイヤーに「0〜2」を入力させる文章を表示させる。
-        # puts "0〜2の数字を入力してください。"
-        # puts "0:グー, 1:チョキ, 2:パー"
-        # 変数「input_hand」にプレイヤーの入力値を代入します。
-      # end if文のend
     end
   end
 end
@@ -52,29 +49,29 @@ end
 class Janken
   def pon(player_hand, enemy_hand)
     # 変数「janken」に["グー", "チョキ", "パー"]を代入します。
-    janken = ["グー", "チョキ", "パー"] #0 : pierre, 1 : papier et 2 : ciseaux
+    janken = ["pierre", "papier", "ciseaux"] #0 : pierre, 1 : papier et 2 : ciseaux
     #「相手の手は#{相手の手}です。」と出力させます。
-    puts
-    puts "相手の手は#{janken[enemy_hand]}です。"
-    diff = player_hand - enemy_hand #difference for player_hand and enemy_hand
-    diff += 3 # difference + 3
-    modulo = diff%3 # (difference + 3) % 3
+    #puts "enemy : #{enemy_hand}"
+    #puts "La main de l'adversiare est #{janken[enemy_hand]}"
     # Playerクラスの戻り値とEnemyクラスの戻り値からじゃんけんするロジックを作成します。
     # Playerクラスの戻り値(player_hand)とEnemyクラスの戻り値(enemy_hand)の値が同じだった場合
-    if modulo == 2
+    if ((player_hand == 0 && enemy_hand == 2) || (player_hand == 1 && enemy_hand == 0) || (player_hand == 2 && enemy_hand == 1))
       # 「あいこ」を出力します。
-      return true
+      puts "La main de l'adversaire est #{janken[enemy_hand]}, vous gagné"
+      return false
       #「true」を返してじゃんけんを繰り返し実行させます。
       # ヒント：「return」を使って戻り値を返すことができます。しかし、Rubyでは戻り値を返す場合、「return」を省略するのが一般的です。
-    elsif modulo == 1 # もしも下記の組み合わせだった場合
+    elsif ((player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)) # もしも下記の組み合わせだった場合
       # (player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0)
       #「あなたの勝ちです」を出力します。
       #「false」を返してじゃんけんを終了させます。
+      puts "La main de l'adversaire est #{janken[enemy_hand]}, vous perdez"
       return false
-    else
+    elsif ((player_hand == 0 && enemy_hand == 0) || (player_hand == 1 && enemy_hand == 1) || (player_hand == 2 && enemy_hand == 2))
       #「あなたの負けです」を出力します。
       #「false」を返してじゃんけんを終了させます。
-       return false
+       puts "La main de l'adversaire est #{janken[enemy_hand]}, c'est un match nul"
+       return true
     end
   end
 end
